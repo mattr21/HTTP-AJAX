277294dd3d29ff9149232684f33d5e503e5fa458
mattr21 <matt.rothstein21@gmail.com>
2019-02-28 13:23:12 -0800

    added changehandler function so our inputs set their values on state

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -9,6 +9,21 @@ class FriendForm extends React.Component {
         }
     }
 
+changeHandler = e => {
+    e.persist();
+    let value = e.target.value;
+    if (e.target.name === 'age') {
+        value = parseInt(value, 10);
+    }
+
+    this.setState(prevState => ({
+        friend: {
+            ...prevState.friend,
+            [e.target.name]: value
+        }
+    }));
+};
+
 render() {
     return (
         <div>
@@ -17,23 +32,24 @@ render() {
                     type="text"
                     name="name"
                     placeholder="Name"
-                    onChange={this.changeHolder}
+                    onChange={this.changeHandler}
                     value={this.state.friend.name}
                 />
                 <input 
                     type="number"
                     name="age"
                     placeholder="Age"
-                    onChange={this.changeHolder}
+                    onChange={this.changeHandler}
                     value={this.state.friend.age}
                 />
                 <input 
                     type="text"
                     name="email"
                     placeholder="Email"
-                    onChange={this.changeHolder}
+                    onChange={this.changeHandler}
                     value={this.state.friend.email}
                 />
+                <button>Submit</button>
             </form>
         </div>
     )
